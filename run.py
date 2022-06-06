35a1cdd57b788e001e0772709ecf9f882883a135
LuQuintas11 <luciaquintas14@gmail.com>
2022-06-06 22:13:05 +0000

    Add parse_menu functions to get the values from worksheet as a dict

--- run.py
+++ run.py
@@ -17,3 +17,14 @@ VEGAN_DATA = SHEET.worksheet("vegan")
 VEGAN = VEGAN_DATA.get_all_values()
 VEGETERIAN_DATA = SHEET.worksheet("vegeterian")
 VEGETERIAN = VEGETERIAN_DATA.get_all_values()
+
+
+def parse_menu(worksheet_data):
+    """
+    Parse values from the different worksheets and create a dict with them
+    """
+    menu_category_dict = defaultdict(list)
+    for row in worksheet_data[1:]:
+        menu_category_dict[row[0]].append(row)
+    return menu_category_dict
+DISH_CATEGORY_MAP = parse_menu(VEGAN)
