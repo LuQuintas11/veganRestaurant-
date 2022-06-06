ec1b60645517287f14052a580ef86034e1988a26
LuQuintas11 <luciaquintas14@gmail.com>
2022-06-06 22:11:47 +0000

    Add scopre and get values from worsheets

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from tabulate import tabulate
+from collections import defaultdict
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("menu")
+VEGAN_DATA = SHEET.worksheet("vegan")
+VEGAN = VEGAN_DATA.get_all_values()
+VEGETERIAN_DATA = SHEET.worksheet("vegeterian")
+VEGETERIAN = VEGETERIAN_DATA.get_all_values()
